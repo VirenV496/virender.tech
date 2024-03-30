806d436e7c14ab2ca228097c2d076a3d1d264955
virender-vishwakarma <virenderv329@gmail.com>
2024-03-30 19:25:23 +0530

    refresh url fix

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import {BrowserRouter} from "react-router-dom";
 function App() {
    return (
       <div>
-         <BrowserRouter basename="/">
+         <BrowserRouter basename="/virender.tech">
             <BaseLayout/>
          </BrowserRouter>
       </div>
