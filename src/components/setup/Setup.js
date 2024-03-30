44a33d507fa2a635978e44e55c68468b3e0294c2
virender-vishwakarma <virenderv329@gmail.com>
2024-03-30 16:14:46 +0530

    first commit

---
+++ src/components/setup/Setup.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Box, } from "@mui/material";
+
+export default function Setup() {
+    return (
+        <Box>
+            <Box>Setup</Box>
+        </Box>
+    );
+};
